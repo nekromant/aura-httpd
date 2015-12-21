5ab29f14316beb77398ba95ec97992d365056cdd
Andrew Andrianov <andrew@ncrmnt.org>
2015-12-21 17:05:44 +0300

    Implement event readout
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- main.c
+++ main.c
@@ -30,12 +30,99 @@ void usage(char *self) {
 }
 
 
-void do_listen_for_events(struct aura_node *n)
+
+static void dump_retbuf(const char *fmt, struct aura_buffer *buf)
 {
-	
+	if (!fmt)
+		return;
+
+	while (*fmt) {
+		switch (*fmt++) { 
+		case URPC_U8: {
+			uint8_t a = aura_buffer_get_u8(buf);
+			printf(" %u ", a);
+			break;
+		}
+ 		case URPC_S8: {
+			int8_t a = aura_buffer_get_s8(buf);
+			printf(" %d ", a);
+			break;
+		}
+		case URPC_U16: {
+			int16_t a = aura_buffer_get_u16(buf);
+			printf(" %u ", a);
+			break;
+		}
+ 		case URPC_S16: {
+			int16_t a = aura_buffer_get_s16(buf);
+			printf(" %d ", a);
+			break;
+		}
+		case URPC_U32: {
+			uint32_t a = aura_buffer_get_u32(buf);
+			printf(" %u ", a);
+			break;
+		}
+
+ 		case URPC_S32: {
+			int32_t a = aura_buffer_get_s32(buf);
+			printf(" %d ", a);
+			break;
+		}
+
+		case URPC_U64: {
+			uint64_t a = aura_buffer_get_u64(buf);
+			printf(" %lu ", a);
+			break;
+		}
+			
+ 		case URPC_S64: {
+			int64_t a = aura_buffer_get_s64(buf);
+			printf(" %ld ", a);
+			break;
+		}
+
+		case URPC_BUF:
+			printf( " buf(0x%lx) ", aura_buffer_get_u64(buf));
+			// TODO: Printout buffer here
+			break;
+
+		case URPC_BIN: {
+			int tmp = atoi(fmt);
+			if (tmp == 0) 
+				BUG(NULL, "Internal serilizer bug processing: %s", fmt);
+			while (*fmt && (*fmt++ != '.'));
+			printf(" bin(%d) ", tmp);
+			break;
+		}
+
+		default:
+			BUG(NULL, "Serializer failed at token: %s", fmt); 
+		}
+	}
 }
 
-void do_call_method(struct aura_node *n, const char *name, const char *cmdlineopts)
+
+static void do_listen_for_events(struct aura_node *n)
+{
+	const struct aura_object *o; 
+	struct aura_buffer *buf;
+	int ret; 
+
+	aura_enable_sync_events(n, 10); 
+
+	while (1) { 		
+		ret = aura_get_next_event(n, &o, &buf); 
+		if (ret != 0)
+			BUG(n, "Failed to read next event");
+		printf("event: %s | ", o->name);
+		dump_retbuf(o->ret_fmt, buf);
+		printf("\n");
+	}
+
+}
+
+static void do_call_method(struct aura_node *n, const char *name, const char *cmdlineopts)
 {
 	
 }
